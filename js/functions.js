a004bb0270d8158fc72ba57a486ddf4c4e95966c
Christina Senina <christina.senina@mail.ru>
2023-10-10 21:50:41 +0300

    Нужно больше функциий

---
+++ js/functions.js
@@ -0,0 +1,70 @@
+//Функция для проверки длины строки
+
+const validateForm = (string, maxLength) => (string.length <= maxLength);
+
+console.log(validateForm('проверяемая строка', 20));
+console.log(validateForm('проверяемая строка', 10));
+console.log(validateForm('проверяемая строка', 18));
+
+//Функция для проверки палиндрома
+
+const isPalindrome = (line) => {
+  const newLine = line.replaceAll(" ", "").toUpperCase();
+  for (let i = 0; i < newLine.length / 2; i++) {
+    if (newLine[i] !== newLine[newLine.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPalindrome('топот'));
+console.log(isPalindrome('ДовОд'));
+console.log(isPalindrome('Кекс'));
+console.log(isPalindrome('Лёша на полке клопа нашёл '));
+
+const isPalindromeByArray = (line) => {
+  const newLine = line.replaceAll(" ", "").toUpperCase();
+  const reverseLine = newLine.split('').reverse().join('');
+  return newLine === reverseLine;
+}
+
+console.log(isPalindromeByArray('топот'));
+console.log(isPalindromeByArray('ДовОд'));
+console.log(isPalindromeByArray('Кекс'));
+console.log(isPalindromeByArray('Лёша на полке клопа нашёл '));
+
+//Функция выявления числа из строки
+
+const getNumber = (string) => {
+  let result = '';
+  const filterString = string.toString();
+  for (let i = 0; i < filterString.length; i++) {
+    if (!Number.isNaN(parseInt(filterString[i]))) {
+      result += filterString[i];
+    }
+  }
+  return parseInt(result);
+}
+
+console.log (getNumber('2023 год'));
+console.log (getNumber('ECMAScript 2022'));
+console.log (getNumber('1 кефир, 0.5 батона'));
+console.log (getNumber('агент 007'));
+console.log (getNumber('а я томат'));
+console.log (getNumber(2023));
+console.log (getNumber(-1));
+console.log (getNumber(1.5));
+
+//при помощи регулярного выражения
+
+const getNumberByRegExp = (value) => parseInt(value.toString().replace(/[^0-9]/g,''));
+
+console.log (getNumberByRegExp('2023 год'));
+console.log (getNumberByRegExp('ECMAScript 2022'));
+console.log (getNumberByRegExp('1 кефир, 0.5 батона'));
+console.log (getNumberByRegExp('агент 007'));
+console.log (getNumberByRegExp('а я томат'));
+console.log (getNumberByRegExp(2023));
+console.log (getNumberByRegExp(-1));
+console.log (getNumberByRegExp(1.5));
---
+++ js/tasks.js
@@ -0,0 +1,83 @@
+const users = [
+  {
+    id: 1,
+    name: 'Aurora',
+    isActive: true,
+    age: 8
+  },
+  {
+    id: 2,
+    name: 'Nessie',
+    isActive: false,
+    age: 23
+  },
+  {
+    id: 3,
+    name: 'Fabbi',
+    isActive: true,
+    age: 20
+  }
+];
+
+function getUsers(arr) {
+  const names = [];
+  for (let i = 0; i < arr.length; i++) {
+    names.push(arr[i].name);
+  }
+  return names;
+}
+
+const getUsersByForEach = (arr) => {
+  const names = [];
+  arr.forEach((item) => {
+    names.push(item.name);
+  });
+  return names;
+};
+
+const getUsersByMap = (arr) => arr.map((user) => user.name);
+
+const getActiveUsers = (arr) => {
+  const activeUsers = [];
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (arr[i].isActive) {
+      activeUsers.push(arr[i].name);
+    }
+  }
+  return activeUsers;
+};
+
+const getActiveUsersByFilter = (arr) => arr.filter((item) => item.isActive).map((element) => element.name);
+
+const sortByAge = (arr) => arr.sort((a, b) => b.age - a.age).map((item) => item.name);
+
+const isNameExist = (arr, name) => arr.some((user) => user.name === name);
+
+console.log(isNameExist(users, 'Fabbi') ? 'There is a user with such a name' : 'No user with such a name');
+
+
+//10.10.23 рисуем
+
+const renderRectangle = (rows, cells) => {
+  let rectangle = "";
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cells; j++) {
+      rectangle = rectangle + "*";
+    }
+    rectangle = rectangle + "\n";
+  }
+  return rectangle;
+};
+
+const renderEmptyRectangle = (rows, cells) => {
+  let rectangle = "";
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cells; j++) {
+      rectangle += i > 0 && i < rows - 1 && j > 0 && j < cells - 1 ? " " : "*";
+    }
+    rectangle = rectangle + "\n";
+  }
+  return rectangle;
+};
+
+console.log(renderEmptyRectangle(5, 10));
