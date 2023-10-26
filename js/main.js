ce48fa5ad982cedafc9480ba7742d6161097aced
Christina Senina <christina.senina@mail.ru>
2023-10-26 21:34:31 +0300

    Создаёт модуль для отрисовки миниатюр

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {getPhotos} from './data.js';
 import {PHOTOS_COUNT} from './constans.js';
+import './miniatures.js';
 
 // eslint-disable-next-line no-console
 console.log(getPhotos(PHOTOS_COUNT));
