9cec1e6705e62b90ab448423657734aebcdec49b
Christina Senina <christina.senina@mail.ru>
2023-11-21 19:43:18 +0300

    Add img types in index.html

--- js/validator.js
+++
@@ -1,8 +0,0 @@
-// import { FILE_TYPES } from './constans';
-
-// const isValidType = (file) => {
-//   const fileName = file.name.toLowerCase();
-//   return FILE_TYPES.some((it) => fileName.endsWith(it));
-// };
-
-// export {isValidType};
