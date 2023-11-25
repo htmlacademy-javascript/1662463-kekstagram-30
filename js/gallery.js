e1b7a5569b8cb90547477934518e5139f1f6857d
Christina Senina <christina.senina@mail.ru>
2023-11-25 15:02:58 +0300

    Вносит изменения в gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -2,19 +2,25 @@ import { showModal } from './modal.js';
 
 const picturesContainer = document.querySelector('.pictures');
 
-const renderGallery = (getPhotos) => {
-  picturesContainer.addEventListener('click', (evt) => {
-    const thumbnail = evt.target.closest('[data-thumbnail-id]');
+const photosArray = [];
 
-    if (!thumbnail) {
-      return;
-    }
-    evt.preventDefault();
-
-    const thumbnailId = + thumbnail.dataset.thumbnailId;
-    const photoData = getPhotos.find(({ id }) => id === thumbnailId);
-    showModal(photoData);
-  });
+const renderGallery = (photos) => {
+  photosArray.length = 0;
+  photosArray.push(...photos.slice());
 };
+
+picturesContainer.addEventListener('click', (evt) => {
+  const thumbnail = evt.target.closest('[data-thumbnail-id]');
+
+  if (!thumbnail) {
+    return;
+  }
+  evt.preventDefault();
+
+  const thumbnailId = + thumbnail.dataset.thumbnailId;
+  const photoData = photosArray.find(({ id }) => id === thumbnailId);
+  showModal(photoData);
+});
+
 
 export { renderGallery };
